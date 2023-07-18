f470cf2b1c798b169e27fc17f2dd4db485505569
joakim000 <joakim.odermalm@gmail.com>
2023-07-18 21:22:44 +0200

    xymon / AllCOL

--- grow/src/ops/xymon.rs
+++ grow/src/ops/xymon.rs
@@ -9,6 +9,12 @@ use std::thread;
 use serde::{Serialize, Deserialize};
 use std::sync::Arc;
 
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct XymonSettings {
+   pub port: u16,
+   pub host: String,
+   pub client: String,
+}
 macro_rules! xymon_match {
     ($x:ident, $y:ident, [$( $variant:tt ),+] ) => {
        match $x {
@@ -30,32 +36,23 @@ macro_rules! xymon_match {
        }
    }
 }
-
+/// Send status update to Xymon server, format:
+/// status[+LIFETIME][/group:GROUP] HOSTNAME.TESTNAME COLOR <additional text>
 pub async fn send_status(data: &ZoneDisplay, x: Arc<XymonSettings>) -> Result<(), Box<dyn Error>> {
-    // status[+LIFETIME][/group:GROUP] HOSTNAME.TESTNAME COLOR <additional text>
-
     let xymon = format!("{}:{}", x.host, x.port);
     let mut stream = match TcpStream::connect(&xymon).await {
         Ok(stream) =>  stream,
         Err(e) => return Err(Box::new(e))
     };
-    // let mut xymon_status = format!("status+1h/group::gardeners {}.", XYMON_CLIENT);
     let mut xymon_status = format!("status+1h {}.", x.client);
     xymon_match!(data, xymon_status, [Water, Air, Light, Aux, Tank, Pump, Arm]);
     match stream.write_all(xymon_status.as_bytes()).await {
-        // Ok(_) => {println!("====== Sent to Xymon: {:?}", &xymon_status);},
+        // Ok(_) => {println!("Sent to Xymon: {:?}", &xymon_status);},
         Ok(_) => (),
         Err(e) => return Err(Box::new(e))
     }
-    let _shut = stream.shutdown();
-    thread::sleep(Duration::from_millis(100));
+    let _ = stream.shutdown();
+    // thread::sleep(Duration::from_millis(10));
     
     Ok(())
-}
-
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct XymonSettings {
-   pub port: u16,
-   pub  host: String,
-   pub  client: String,
 }
